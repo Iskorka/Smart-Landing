8a2a4cd29faa6c133a7a5b5c86f828d3c3d5ace3
Iskorka <camplesjeydi000@gmail.com>
2017-09-10 17:51:09 +0300

    Add 'Services' block and JS code for top indents

--- js/script.js
+++ js/script.js
@@ -4,11 +4,33 @@ $(document).ready(function(){
 		selector.height(bannerWidth);
 	}
 	
+	function getTopIndent(indent){
+		return $('.wrap').width()/100*indent;
+	}
+	
+	
 	setBannerHeight($('.work-process li'));
 	setBannerHeight($('.featured-work li'));
+	$('.services li').height($('.services li').width()/100*90);
+	$('.btn').height($('.btn').width()/100*20.833333333);
+	
+	$('.work-process').css('top', getTopIndent(63.888888888));
+	$('.featured-work').css('top', getTopIndent(117.361111111));
+	$('header').height(getTopIndent(63.888888888));
+	$('form').height($('form').width()/100*105.405405405);
+	
 	
 	$( window ).resize(function() {
 		setBannerHeight($('.work-process li'));
 		setBannerHeight($('.featured-work li'));
+		$('.services li').height($('.services li').width()/100*90);
+		$('.btn').height($('.btn').width()/100*20.833333333);
+		
+		$('.work-process').css('top', getTopIndent(63.888888888));
+		$('.featured-work').css('top', getTopIndent(117.361111111)); /* 1690px */
+		$('.services').css('top', getTopIndent(231.458333333));
+		
+		$('header').height(getTopIndent(63.888888888));
+		$('form').height($('form').width()/100*105.405405405);
 	});
 });
