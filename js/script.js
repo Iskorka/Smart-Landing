8d44c714f01b785e774d0c1f6518dbe151065594
Iskorka <camplesjeydi000@gmail.com>
2017-09-10 13:10:17 +0300

    Add 'Working Process' and 'Featured Work' blocks

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+	function setBannerHeight(selector){
+		var bannerWidth = selector.width();
+		selector.height(bannerWidth);
+	}
 	
+	setBannerHeight($('.work-process li'));
+	setBannerHeight($('.featured-work li'));
+	
+	$( window ).resize(function() {
+		setBannerHeight($('.work-process li'));
+		setBannerHeight($('.featured-work li'));
+	});
 });
